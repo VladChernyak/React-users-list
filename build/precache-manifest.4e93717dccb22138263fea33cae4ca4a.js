37037f9e740a176820ea030c5dfee543684bf8ea
Vlad Chernyak <VladChernyak@users.noreply.github.com>
2020-07-26 21:08:34 +0300

    add build

---
+++ build/precache-manifest.4e93717dccb22138263fea33cae4ca4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd87125e75b70ad314ae24f8062bfb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7c8655ac0078bfff973",
+    "url": "/static/css/main.dab1464a.chunk.css"
+  },
+  {
+    "revision": "ba4efdb2038ca9d51b4c",
+    "url": "/static/js/2.7fe879c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7fe879c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7c8655ac0078bfff973",
+    "url": "/static/js/main.7c989bad.chunk.js"
+  },
+  {
+    "revision": "ce02b4d83709bae1d85d",
+    "url": "/static/js/runtime-main.5566532b.js"
+  }
+]);
